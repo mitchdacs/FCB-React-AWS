b79af4ce8e2b80d27ad76bd6e94f946444a7b228
Mitch Dacs <misyell.homebased@gmail.com>
2022-01-21 14:41:18 +0800

    Organize the file and folder structure of components

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import TasksList from "./components/TasksList";
 
-function Task(props){
-	return(
-		<li className="tasks-item">{props.taskName}</li>
-	);
-}
 
-function TasksList(){
-	const taskItemsList = [
-		"Follow Edukasyon.ph on Facebook",
-		"Follow AWS Siklab Pilipinas on Facebook",
-		"Follow Zuitt Coding Bootcamp on Facebook"
-	];
-
-	return (
-		<div>
-			<input className="task-input" />
-			<ul>
-				{taskItemsList.map((task, index) => {
-					return <Task key={index} taskName={task} />
-				})}
-			</ul>
-		</div>
-	);	
-}
 
 function App(){
 	const pStyle = {
