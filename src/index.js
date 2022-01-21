e21590f9f8aa2db49857f59dc75310feeac7c5db
Mitch Dacs <misyell.homebased@gmail.com>
2022-01-21 13:53:32 +0800

    Create a container for task items

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ function Task(props){
 }
 
 function TasksList(){
+	const taskItemsList = [
+		"Follow Edukasyon.ph on Facebook",
+		"Follow AWS Siklab Pilipinas on Facebook",
+		"Follow Zuitt Coding Bootcamp on Facebook"
+	];
+
 	return (
 		<ul>
-			<Task taskName="Follow Edukasyon.ph on Facebook" />
-			<Task taskName="Follow AWS Siklab Pilipinas on Facebook" />			
-			<Task taskName="Follow Zuitt Coding Bootcamp on Facebook" />
+			{taskItemsList.map((task, index) => {
+				return <Task key={index} taskName={task} />
+			})}
 		</ul>
 	);	
 }
