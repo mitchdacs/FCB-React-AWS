7b477a962ff7111abe7aad77a0707f8872d7f6a2
Mitch Dacs <misyell.homebased@gmail.com>
2022-01-21 14:08:12 +0800

    Add an input box

--- src/index.js
+++ src/index.js
@@ -16,11 +16,14 @@ function TasksList(){
 	];
 
 	return (
-		<ul>
-			{taskItemsList.map((task, index) => {
-				return <Task key={index} taskName={task} />
-			})}
-		</ul>
+		<div>
+			<input className="task-input" />
+			<ul>
+				{taskItemsList.map((task, index) => {
+					return <Task key={index} taskName={task} />
+				})}
+			</ul>
+		</div>
 	);	
 }
 
